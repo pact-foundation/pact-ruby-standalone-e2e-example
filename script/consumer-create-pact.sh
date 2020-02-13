053e6f0ffcc65fa60bbff774a689319081304f09
Beth Skurrie <beth@bethesque.com>
2020-02-13 14:47:25 +1100

    chore: run mock service over https

--- script/consumer-create-pact.sh
+++ script/consumer-create-pact.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
 # start mock service
-pact/bin/pact-mock-service start -p 1234 --pact-dir ./pacts --log ./log/foo-bar-mock-service.log
+pact/bin/pact-mock-service start -p 1234 --pact-dir ./pacts --ssl --sslcert support/self-signed-cert.pem --sslkey support/self-signed-key.pem --log ./log/foo-bar-mock-service.log
 
+export SSL_CERT_FILE=support/self-signed-cert.pem
 # clear interactions (not necessary for this example, just showing how)
-curl -X DELETE -H "X-Pact-Mock-Service: true"  localhost:1234/interactions
+curl -X DELETE -H "X-Pact-Mock-Service: true"  https://localhost:1234/interactions
 
 # set up interaction
-curl -X POST -H "X-Pact-Mock-Service: true" -d@script/consumer-interaction.json localhost:1234/interactions
+curl -X POST -H "X-Pact-Mock-Service: true" -d@script/consumer-interaction.json https://localhost:1234/interactions
 
 # execute interaction
-curl localhost:1234/foo
+curl https://localhost:1234/foo
 
 # verify interaction took place
-curl -H "X-Pact-Mock-Service: true" localhost:1234/interactions/verification
+curl -H "X-Pact-Mock-Service: true" https://localhost:1234/interactions/verification
 
 # write pact
-curl -X POST -H "X-Pact-Mock-Service: true" -d '{"consumer": {"name": "Foo"}, "provider": {"name": "Bar"}}' localhost:1234/pact
+curl -X POST -H "X-Pact-Mock-Service: true" -d '{"consumer": {"name": "Foo"}, "provider": {"name": "Bar"}}' https://localhost:1234/pact
 
 # stop mock service
 pact/bin/pact-mock-service stop -p 1234
