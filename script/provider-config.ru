71a985b785fa3f7a01ed5762c3fa5648c10b54b9
Beth Skurrie <beth@bethesque.com>
2020-04-25 10:15:17 +1000

    feat: show that multiple --custom-provider-header flags are supported

--- script/provider-config.ru
+++ script/provider-config.ru
@@ -1,6 +1,8 @@
 require 'json'
 
 run -> (env) {
+  puts "Foo1 header #{env['HTTP_FOO1']}"
+  puts "Foo2 header #{env['HTTP_FOO2']}"
   status = 200
   headers = {'Content-Type' => 'application/json'}
   body = {'message' => 'Hello world'}.to_json
--- script/provider-verify-pact.sh
+++ script/provider-verify-pact.sh
@@ -5,7 +5,8 @@ pact/bin/rackup script/provider-config.ru -p 9292 &
 provider_pid="$!"
 
 # verify pact
-pact/bin/pact-provider-verifier verify --pact-urls pacts/foo-bar.json --provider-base-url http://localhost:9292
+pact/bin/pact-provider-verifier verify pacts/foo-bar.json --provider-base-url http://localhost:9292 \
+   --custom-provider-header Foo1:bar1 --custom-provider-header Foo2:bar2
 
 # stop provider server
 kill -9 $provider_pid
